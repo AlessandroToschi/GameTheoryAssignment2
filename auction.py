3158bab945e4f2b4ba00633256b43fba155588ad
Alessandro Toschi <alessandro@MacBook-Pro-di-Alessandro.local>
2018-09-26 10:35:00 +0800

    added english auction with simulation

--- auction.py
+++ auction.py
@@ -1,4 +1,5 @@
-import buyer
+from buyer import Buyer, Distribution, OfferResult
+import threading
 
 BUYERS = 10
 
@@ -8,9 +9,74 @@ class Auction(object):
             self.__reserve_price = reserve_price
         else:
             raise ValueError("Reserve price must be greater or equal to zero.")
-        self.__buyers = [Buyer() for _ in range(BUYERS)]
+        self._buyers = self.__create_buyers()
+        self._lock = threading.Lock()
+    
+    def __create_buyers(self):
+        buyers = [Buyer(ID + 1) for ID in range(BUYERS)]
+        while(len(set([x.value for x in buyers])) < BUYERS):
+            buyers = [Buyer(ID + 1) for ID in range(BUYERS)]
+        return buyers
     
     def simulate(self):
         NotImplementedError("This method can be called only by specialized children.")
     
     reserve_price = property(lambda self: self.__reserve_price)
+
+class EnglishAuction(Auction):
+    def __init__(self, reserve_price=0):
+        Auction.__init__(self, reserve_price=reserve_price)
+    
+    def offer_pooling(self, queue, current_value, buyer):
+        result, value = buyer.raise_offer(current_value)
+        if result == OfferResult.RAISE:
+            with self._lock:
+                queue.append((buyer.ID, result, value))
+
+    def simulate(self):
+        stop = False
+        current_offer = 0
+        results = []
+        threads = []
+        max_offering = 0
+        max_buyer_offering = ""
+        print("English auction is ready to start, with a starting offer of 0.")
+        print("10 buyers are attending.")
+        while(stop == False):
+            results.clear()
+            threads.clear()
+            print("Who offers " + str(current_offer + 1) + "?")
+            for buyer in self._buyers:
+                buyer_thread = threading.Thread(target=self.offer_pooling, args=(results, current_offer, buyer))
+                threads.append(buyer_thread)
+                buyer_thread.start()
+            for thread in threads:
+                thread.join()
+            if len(results) == 0:
+                if max_offering > self.reserve_price:
+                    print("Player {} has won the auction with an offer of {}!".format(max_buyer_offering, max_offering))
+                else:
+                    print("The trade can't be done, the max offer is {} but the reserve price is {}".format(max_offering, self.reserve_price))
+                stop = True
+            else:
+                if max_buyer_offering == results[0][0] and len(results) == 1:
+                    if max_offering > self.reserve_price:
+                        print("Player {} has won the auction with an offer of {}!".format(max_buyer_offering, max_offering))
+                    else:
+                        print("The trade can't be done, the max offer is {} but the reserve price is {}".format(max_offering, self.reserve_price))
+                    stop = True
+                    continue
+                print("Player {} has offered {}".format(results[0][0], results[0][2]))
+                max_offering = results[0][2]
+                max_buyer_offering = results[0][0]
+                current_offer = max_offering
+            print("\n")
+        for buyer in self._buyers:
+            print("{} {}".format(buyer.ID, buyer.value))
+            
+    
+
+
+if __name__ == "__main__":
+    ea = EnglishAuction(500)
+    ea.simulate()
--- buyer.py
+++ buyer.py
@@ -1,8 +1,9 @@
 import numpy as np
 from enum import IntEnum
+from time import sleep
 
 COMMON_VALUE = 100
-STANDARD_DEVIATION = 15.0
+STANDARD_DEVIATION = 20.0
 
 class OfferResult(IntEnum):
     RAISE = 1
@@ -13,24 +14,27 @@ class Distribution(IntEnum):
     UNIFORM = 2
 
 class Buyer(object):
-    def __init__(self, common_value=100, distribution=Distribution.GAUSSIAN):
+    def __init__(self, ID, common_value=100, distribution=Distribution.GAUSSIAN):
         if distribution == Distribution.GAUSSIAN:
             self.__value = round(common_value + np.random.normal(loc=0, scale=STANDARD_DEVIATION))
         elif distribution == Distribution.UNIFORM:
             self.__value = round(common_value + np.random.uniform(-STANDARD_DEVIATION, STANDARD_DEVIATION + 1.0))
         else:
             self.__value = round(common_value)
+        self.__ID = ID
     
     def raise_offer(self, current_value):
+        sleep(float(np.random.randint(50, 300)) / 1000.0)
         if current_value < self.__value:
-            return (RAISE, current_value + 1)
+            return (OfferResult.RAISE, current_value + 1)
         else:
-            return (LEAVE, 0)
+            return (OfferResult.LEAVE, 0)
 
     value = property(lambda self: self.__value)
+    ID = property(lambda self: self.__ID)
 
 
 if __name__ == "__main__":
-    x = [Buyer().value for _ in range(10)]
+    x = [Buyer(i + 1).value for i in range(10)]
     print(x)
     print(len(set(x)))
